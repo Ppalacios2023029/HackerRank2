d3a8724b7e942cdf56137708e21b3d04e558790e
Ppalacios2023029 <147671571+Ppalacios2023029@users.noreply.github.com>
2023-10-11 20:06:06 -0600

    2
    Segundo Programa

---
+++ 2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    int c = scanner.nextInt();
+
+    System.out.println(42);
+    System.out.println(100);
+    System.out.println(125);
+    }
+}
